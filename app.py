6aad10d51bcb7905b9296cd6e3aeb3b2be41cb0e
seongyeon574 <71360754+seongyeon574@users.noreply.github.com>
2020-11-18 21:33:02 +0900

    final

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@54.180.85.97', 27017)
 db = client.traveldb
 
 
